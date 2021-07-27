b041876c5986581353341ff4a34193fbcdace23f
Rodrigo de Lima Oliveira <rodrigolima82@gmail.com>
2021-07-26 21:36:36 -0300

    Update infra.tf

--- terraform/infra.tf
+++ terraform/infra.tf
@@ -10,8 +10,8 @@ provider "google" {
   region = "us-central1"
 }
 
-resource "google_storage_bucket" "funky-bucket" {
-  name = "batch-ingestion-pipeline-poc"
-  storage_class = "REGIONAL"
-  location  = "us-central1"
-}
+#resource "google_storage_bucket" "funky-bucket" {
+#  name = "batch-ingestion-pipeline-poc"
+#  storage_class = "REGIONAL"
+#  location  = "us-central1"
+#}
